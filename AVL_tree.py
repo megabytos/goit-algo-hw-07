b0cb93f553036bfd20e447334888e2b302a8ec36
Alex <alx3@i.ua>
2024-01-31 23:05:39 +0200

    hw7

---
+++ AVL_tree.py
@@ -0,0 +1,185 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import uuid
+
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.key) 
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
+
+
+def draw_tree(tree_root):
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)} 
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2000, node_color=colors)
+    plt.show()
+
+
+class AVLNode:
+    def __init__(self, key):
+        self.key = key
+        self.height = 1
+        self.left = None
+        self.right = None
+        self.color = "skyblue"
+        self.id = str(uuid.uuid4())
+
+
+def get_height(node):
+    if not node:
+        return 0
+    return node.height
+
+def get_balance(node):
+    if not node:
+        return 0
+    return get_height(node.left) - get_height(node.right)
+
+def left_rotate(z):
+    if z is None or z.right is None:
+            return z
+    y = z.right
+    T2 = y.left
+    y.left = z
+    z.right = T2
+    z.height = 1 + max(get_height(z.left), get_height(z.right))
+    y.height = 1 + max(get_height(y.left), get_height(y.right))
+    return y
+
+def right_rotate(y):
+    if y is None or y.left is None:
+            return y
+    x = y.left
+    T3 = x.right
+    x.right = y
+    y.left = T3
+    y.height = 1 + max(get_height(y.left), get_height(y.right))
+    x.height = 1 + max(get_height(x.left), get_height(x.right))
+    return x
+
+def min_value_node(node):
+    current = node
+    while current.left is not None:
+        current = current.left
+    return current
+
+def max_value_node(node):
+    current = node
+    while current.right is not None:
+        current = current.right
+    return current
+
+def sum_nodes_values(root):
+    if root is None:
+        return 0
+    if root:
+	    return sum_nodes_values(root.left) + sum_nodes_values(root.right) + root.key   
+
+def insert_node(root, key):
+    if not root:
+        return AVLNode(key)
+
+    if key < root.key:
+        root.left = insert_node(root.left, key)
+    elif key > root.key:
+        root.right = insert_node(root.right, key)
+    else:
+        return root
+
+    root.height = 1 + max(get_height(root.left), get_height(root.right))
+
+    balance = get_balance(root)
+
+    if balance > 1:
+        if key < root.left.key:
+            return right_rotate(root)
+        else:
+            root.left = left_rotate(root.left)
+            return right_rotate(root)
+
+    if balance < -1:
+        if key > root.right.key:
+            return left_rotate(root)
+        else:
+            root.right = right_rotate(root.right)
+            return left_rotate(root)
+
+    return root
+
+def delete_node(root, key):
+    if not root:
+        return root
+
+    if key < root.key:
+        root.left = delete_node(root.left, key)
+    elif key > root.key:
+        root.right = delete_node(root.right, key)
+    else:
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+
+        temp = min_value_node(root.right)
+        root.key = temp.key
+        root.right = delete_node(root.right, temp.key)
+
+    if root is None:
+        return root
+
+    root.height = 1 + max(get_height(root.left), get_height(root.right))
+
+    balance = get_balance(root)
+
+    if balance > 1:
+        if get_balance(root.left) >= 0:
+            return right_rotate(root)
+        else:
+            root.left = left_rotate(root.left)
+            return right_rotate(root)
+
+    if balance < -1:
+        if get_balance(root.right) <= 0:
+            return left_rotate(root)
+        else:
+            root.right = right_rotate(root.right)
+            return left_rotate(root)
+
+    return root
+
+
+if __name__ == "__main__":    
+    root = None
+    keys_to_insert = [5, 20, 10, 32, 25, 28, 27, -3, 128]
+    for key in keys_to_insert:
+        root = insert_node(root, key)
+    
+    keys_to_delete = [10, 27]
+    for key in keys_to_delete:
+        root = delete_node(root, key)
+
+    print(f"Найменше значення у дереві: {min_value_node(root).key}")
+    print(f"Найбільше значення у дереві: {max_value_node(root).key}")
+    print(f"Сума всіх значень у дереві: {sum_nodes_values(root)}")
+
+    draw_tree(root)    
